05bc992f188580c3df98953b695cc478cc0c4019
Uhut Sancar <sancaruhut@gmail.com>
2025-05-04 22:52:22 +0300

    account page added

--- src/main/components/account/account.component.ts
+++ src/main/components/account/account.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../login/auth.service';
+import { AuthApi } from '../../data-access/auth-api';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent  {
 
-  constructor() { }
+  authApi = inject(AuthApi);
+  authService = inject(AuthService);
 
-  ngOnInit() {
-  }
+
+
+
+
 
 }
